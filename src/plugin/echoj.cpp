9e8275b9c5f8a349a4a127365f653cff3aa4a1cb
Paolo Bosetti <paolo.bosetti@unitn.it>
2024-02-08 15:54:50 +0100

    Cleanup and examples

--- src/plugin/echoj.cpp
+++ src/plugin/echoj.cpp
@@ -19,6 +19,9 @@ It takes a JSON input and returns it as output, enriched with the parameters
 using namespace std;
 using json = nlohmann::json;
 
+
+// Plugin class. This shall be the only part that needs to be modified,
+// implementing the actual functionality
 class Echo : public Filter<json, json> {
 public:
   string kind() override { return PLUGIN_NAME; }
@@ -38,13 +41,16 @@ private:
   json _data, _params;
 };
 
+
+
 /*
   ____  _             _             _      _                
  |  _ \| |_   _  __ _(_)_ __     __| |_ __(_)_   _____ _ __ 
  | |_) | | | | |/ _` | | '_ \   / _` | '__| \ \ / / _ \ '__|
  |  __/| | |_| | (_| | | | | | | (_| | |  | |\ V /  __/ |   
  |_|   |_|\__,_|\__, |_|_| |_|  \__,_|_|  |_| \_/ \___|_|   
-                |___/                                       
+                |___/                                      
+This is the plugin driver, it should not need to be modified 
 */
 
 class EchoDriver : public FilterDriver<json, json> {
@@ -57,6 +63,8 @@ extern "C" EXPORTIT void register_pugg_plugin(pugg::Kernel *kernel) {
   kernel->add_driver(new EchoDriver());
 }
 
+
+
 /*
                   _
   _ __ ___   __ _(_)_ __
@@ -64,12 +72,29 @@ extern "C" EXPORTIT void register_pugg_plugin(pugg::Kernel *kernel) {
  | | | | | | (_| | | | | |
  |_| |_| |_|\__,_|_|_| |_|
 
+For testing purposes, when directly executing the plugin
 */
-
+#include <sstream>
 int main(int argc, char const *argv[]) {
   Echo echo;
   json params = {{"times", 3}};
   json data = {{"array", {1, 2, 3, 4}}};
+
+  // Parse input: with - swutch, read from stdin a single JSON object with
+  // "data" and "params" keys
+  if (argc > 1 && argv[1] == string("-")) {
+    stringstream ss;
+    while (cin >> ss.rdbuf()) {}
+    json j = json::parse(ss.str());
+    data = j["data"];
+    params = j["params"];
+  } else if (argc > 1) {
+    cout << "Usage: " << argv[0] << " [-]" << endl;
+    cout << "       use the optional '-' to read input from stdin" << endl;
+    return 1;
+  }
+
+  // Process data
   echo.set_params(&params);
   echo.load_data(data);
   json result;
@@ -77,6 +102,8 @@ int main(int argc, char const *argv[]) {
     cerr << "Error processing data" << endl;
     return 1;
   }
+
+  // Produce output
   cout << "Input: " << data << endl;
   cout << "Output: " << result << endl;
 
