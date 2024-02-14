2ea8536624e700033332cbd116ac76080a1ca906
Paolo Bosetti <paolo.bosetti@unitn.it>
2024-02-14 14:15:32 +0100

    webcam plugin has histogram

--- src/plugin/webcam.cpp
+++ src/plugin/webcam.cpp
@@ -11,6 +11,7 @@ Produces current time and date
 #include "../source.hpp"
 #include <chrono>
 #include <nlohmann/json.hpp>
+#include <opencv2/imgproc.hpp>
 #include <opencv2/opencv.hpp>
 #include <pugg/Kernel.h>
 #include <sstream>
@@ -32,16 +33,17 @@ public:
   Webcam() {
     _error = "none";
     _blob_format = "jpg";
-  } 
+  }
 
   ~Webcam() {
-    cap.release();
+    _cap.release();
     destroyAllWindows();
   }
 
   string kind() override { return PLUGIN_NAME; }
 
-  static string get_ISO8601(const system_clock::time_point &time = chrono::system_clock::now()) {
+  static string get_ISO8601(
+      const system_clock::time_point &time = chrono::system_clock::now()) {
     time_t tt = system_clock::to_time_t(time);
     tm *tt2 = localtime(&tt);
 
@@ -59,20 +61,40 @@ public:
         .str();
   }
 
-  return_type get_output(json *out, std::vector<unsigned char> *blob = nullptr) override {
+  return_type get_output(json *out,
+                         std::vector<unsigned char> *blob = nullptr) override {
     return_type result = return_type::success;
+    Mat hist;
+    int hist_size = 20;
+    if (_params.contains("hist_size")) {
+      hist_size = _params["hist_size"];
+    }
     while (true) {
-      cap >> frame;
-      if (frame.empty())
+      _cap >> _frame;
+      hist.release();
+      if (_frame.empty())
         return return_type::error;
-      resize(frame, frame, Size(), 0.3, 0.3);
-      putText(frame, get_ISO8601(), Point{5, frame.rows - 5}, 0, 0.5, 1, 1, 8, false);
-      imshow("frame", frame);
-      char k = waitKey(1000.0/25);
-      if(' ' == k) {
-        imwrite(_params["image_name"], frame);
+      resize(_frame, _frame, Size(), 0.3, 0.3);
+      cvtColor(_frame, _gray, COLOR_BGR2GRAY);
+      calcHist(&_gray,
+               1,         // number of images
+               0,         // channels
+               Mat(),     // mask
+               hist,      // histogram
+               1,         // dimensionality
+               &hist_size, // number of bins
+               0);
+      normalize(hist, hist, 0, 255, NORM_MINMAX, CV_32F);
+
+      putText(_frame, get_ISO8601(), Point{5, _frame.rows - 5}, 0, 0.5, 1, 1, 8,
+              false);
+      imshow("frame", _frame);
+      // imshow("Histogram", hist_image);
+      char k = waitKey(1000.0 / 25);
+      if (' ' == k) {
+        imwrite(_params["image_name"], _frame);
         if (blob) {
-          imencode("." + _blob_format, frame, *blob);
+          imencode("." + _blob_format, _frame, *blob);
         }
         break;
       } else if ('q' == k) {
@@ -82,26 +104,30 @@ public:
       }
     }
     if (return_type::success == result) {
-      (*out)["frame_size"] = {{cvRound(cap.get(CAP_PROP_FRAME_WIDTH)), cvRound(cap.get(CAP_PROP_FRAME_HEIGHT))}};
-      (*out)["image_size"] = {{frame.cols, frame.rows}};
+      (*out)["frame_size"] = {{cvRound(_cap.get(CAP_PROP_FRAME_WIDTH)),
+                               cvRound(_cap.get(CAP_PROP_FRAME_HEIGHT))}};
+      (*out)["image_size"] = {{_frame.cols, _frame.rows}};
+      (*out)["histogram"] = json::array();
+      for (int i = 0; i < hist_size; i++) {
+        (*out)["histogram"].push_back(cvRound(hist.at<float>(i)));
+      }
     }
     return result;
   }
 
-  void set_params(void *params) override { 
-    _params = *(json *)params; 
+  void set_params(void *params) override {
+    _params = *(json *)params;
     int device = _params["device"];
-    cap.open(device, CAP_AVFOUNDATION);
-    if (!cap.isOpened()) {
+    _cap.open(device, CAP_AVFOUNDATION);
+    if (!_cap.isOpened()) {
       throw runtime_error("Error: Unable to open webcam");
     }
-    
   }
 
 private:
   json _data, _params;
-  Mat frame;
-  VideoCapture cap;
+  Mat _frame, _gray;
+  VideoCapture _cap;
 };
 
 /*
@@ -136,19 +162,20 @@ For testing purposes, when directly executing the plugin
 int main(int argc, char const *argv[]) {
   Webcam wc;
   json output;
-  vector<unsigned char> blob;  
-  wc.set_params(new json({{"device", 0}, {"image_name", "image.jpg"}}));
+  vector<unsigned char> blob;
+  wc.set_params(new json({
+    {"device", 0}, 
+    {"image_name", "image.jpg"}}));
 
   auto now = system_clock::now();
   auto today = floor<days>(now);
   auto tod = duration_cast<seconds>(now - today);
   cout << "Local time: " << tod.count() << endl;
 
-  wc.get_output(&output, &blob);
-
-  cout << "Webcam: " << output.dump() << endl;
-  cout << "Image size: " << blob.size() << " bytes" << endl;
-  
+  while (wc.get_output(&output, &blob) == return_type::success) {
+    cout << "Webcam: " << output.dump() << endl;
+    cout << "Image size: " << blob.size() << " bytes" << endl;
+  }
 
   return 0;
 }
